f28c92b7ffb2d65a404f8be2069124d5f57c3529
unknown <jyoti.sin1006@gmail.com>
2026-01-25 01:46:17 +0530

    25thjn commit

--- Day-13-RDS-created-secretmanager/main.tf
+++ Day-13-RDS-created-secretmanager/main.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "default" {
   allocated_storage       = 10
-   identifier =             "book-rds"
+  identifier =             "book-rds"
   db_name                 = "mydb"
   engine                  = "mysql"
   engine_version          = "8.0"
@@ -9,15 +9,15 @@ resource "aws_db_instance" "default" {
   username                    = "admin"
   db_subnet_group_name    = aws_db_subnet_group.sub-grp.id
   parameter_group_name    = "default.mysql8.0"
-  backup_retention_period  = 7   # Retain backups for 7 days
+  #backup_retention_period  = 7   # Retain backups for 7 days
   backup_window            = "02:00-03:00" # Daily backup window (UTC)
 
   # Enable performance insights
 #   performance_insights_enabled          = true
 #   performance_insights_retention_period = 7  # Retain insights for 7 days
   maintenance_window = "sun:04:00-sun:05:00"  # Maintenance every Sunday (UTC)
-  deletion_protection = true
-  skip_final_snapshot = true
+  skip_final_snapshot = false
+  deletion_protection = false
   depends_on = [ aws_db_subnet_group.sub-grp ]
 }
 
